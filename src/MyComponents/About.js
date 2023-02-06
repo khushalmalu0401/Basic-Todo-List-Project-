60794d51c72602e620f02ee301b75cece24160ef
Khushal <khushalmalu04@gmail.com>
2023-02-06 23:57:20 +0530

    Commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+      This is about component
+    </div>
+  )
+}
+
---
+++ src/MyComponents/Todos.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { TodoItem } from "./TodoItem";
+export const Todos = (props) => {
+  let myStyle = {
+    minHeight: "70vh",
+    margin: "40px auto",
+  };
+  return (
+    <div className="container" style={myStyle}>
+      <h3 className="text-center my-3">Todos List</h3>
+      {props.todos.length === 0 ? (
+        <h3>No Todos to Display</h3>
+      ) : (
+        props.todos.map((todo) => {
+          return (
+            <>
+              <TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete} /><hr />
+              </>
+          );
+        })
+      )}
+    </div>
+  );
+};
+//This contains how to add style in react using style={myStyle} & then including the CSS using the object
